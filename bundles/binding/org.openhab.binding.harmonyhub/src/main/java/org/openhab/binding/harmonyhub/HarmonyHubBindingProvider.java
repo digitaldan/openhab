cdcfcd87d8ac9ec58fc928a734e47ef281ad6d48
daniel <daniel@osxlaptop.local>
2014-11-14 08:01:19 -0700

    inital commit of harmony hub, still need to fix maven and osgi library imports

---
+++ bundles/binding/org.openhab.binding.harmonyhub/src/main/java/org/openhab/binding/harmonyhub/HarmonyHubBindingProvider.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.harmonyhub;
+
+import org.openhab.binding.harmonyhub.internal.HarmonyHubBinding;
+import org.openhab.core.binding.BindingProvider;
+
+/**
+ * @author Matt Tucker
+ * @since 1.5.1
+ */
+public interface HarmonyHubBindingProvider extends BindingProvider {
+    boolean isInBound(String itemName);
+    void bind(HarmonyHubBinding harmonyHubBinding, String itemName);
+}
